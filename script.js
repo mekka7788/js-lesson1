c3d90f1a556f1654d4770bbb266d2d80f453ff1c
mekka7788 <meerimaalisherova@gmail.com>
2023-07-10 10:18:30 +0300

    my first commit js lesson

---
+++ script.js
@@ -0,0 +1,44 @@
+// let a = 5
+// console.log(a)
+/*
+let a = "Hello"
+let b ="Mээрим"
+let с = ","
+let d =35
+console.log(a+" "+b+с+" "+d)
+*/
+/*let a =  4
+let b = 55
+console.log(a+b)*/
+
+// let a = 4
+// let b = 55
+// // console.log(a*b)
+// let a = 4
+// let b = 55
+// console.log(b/a)
+// let a = 4
+// let b = 55
+// console.log(b%a)
+// let a = 4
+// let b = 55
+// console.log(a % b)
+// let a = "Hello"
+// let b = "Amirhan"
+// console.log(a+" "+b)
+// let a = 88
+// let b = "Mekka"
+// console.log(a/b)
+// let a = "Mekka"
+// let b = "Rozan"
+// console.log(a*b)
+// let a = 5
+// let b = "5"
+// console.log(a+ +b)
+// let a = 5
+// // let b = "5"
+// // console.log(a+Number(b))
+// let c = 2
+// let b = "undefined"
+// console.log(c+b)
+
